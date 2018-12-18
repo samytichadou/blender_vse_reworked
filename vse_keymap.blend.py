5b96ae6cf690c33e0e8adbcc668d8bf82951d3ad
tin2tin <tintwotin@gmail.com>
2018-12-18 22:50:47 +0100

    View channels alt+ctrl + a/s

--- vse_keymap.blend.py
+++ vse_keymap.blend.py
@@ -630,13 +630,26 @@ keyconfig_data = \
       },
      ),
     ("sequencer.view_channel",
-     {"type": 'A', "value": 'PRESS', "shift": True, "alt": True},
+     {"type": 'A', "value": 'PRESS', "ctrl": True, "alt": True},
      {"properties":
       [("type", 'ALL'),
        ],
       },
      ),
     ("sequencer.view_channel",
-     {"type": 'S', "value": 'PRESS', "shift": True, "alt": True},
+     {"type": 'S', "value": 'PRESS', "ctrl": True, "alt": True},
      {"properties":
-      [
+      [("type", 'SOLO'),
+       ],
+      },
+     ),
+    ],
+   },
+  ),
+ ]
+
+
+if __name__ == "__main__":
+    import os
+    from bl_keymap_utils.io import keyconfig_import_from_data
+    keyconfig_import_from_data(os.path.splitext(os.path.basename(__file__))[0], keyconfig_data)
